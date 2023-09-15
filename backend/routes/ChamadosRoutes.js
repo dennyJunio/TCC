4b7486b68f357100b14f4c65695228d9ddb2e6d4
Denny Junio Costa Coimbra - SESI CUIABA-MT <denny.coimbra@alunosesi.mt>
2023-09-15 12:18:32 -0400

    apenas salvando

--- backend/routes/ChamadosRoutes.js
+++ backend/routes/ChamadosRoutes.js
@@ -6,5 +6,7 @@ const verifyToken = require('../helpers/verify-token')
 //---------------- rotas privadas ----------------
 router.post('/create', verifyToken, ChamadosController.create)
 router.get('/getall', verifyToken, ChamadosController.getAll)
+router.get('/mychamados', verifyToken, ChamadosController.getAllUserChamados)
+router.patch('/editar/:id', verifyToken, ChamadosController.updateChamados)
 
 module.exports = router
