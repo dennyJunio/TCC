6c44ba880155b22b2f9b88a5de934b524a5b7ded
Daniela <danielaludwigmt@gmail.com>
2023-10-03 16:30:51 -0400

    salvando para mudar de branch

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,7 +2,7 @@
 
 const { Sequelize } = require('sequelize')
 //troca a senha quando tiver no meu PC
-const sequelize = new Sequelize('supMate', 'root', 'sucesso', {
+const sequelize = new Sequelize('supMate', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 })
